1279e83cfc0eb26b24b9ff96e94d1695102a6629
Akash Chaudhary <58812239+cyberlord-007@users.noreply.github.com>
2020-05-13 09:32:26 +0530

    Updates

---
+++ precache-manifest.b299103fd7bf6fc58fb1640d9d2438bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f1f180231669e9c7344edc806dcca6",
+    "url": "/CodeFlex/index.html"
+  },
+  {
+    "revision": "b22a4b565141362617fd",
+    "url": "/CodeFlex/static/css/main.b2351552.chunk.css"
+  },
+  {
+    "revision": "3eae9c21a5896f5b4a3b",
+    "url": "/CodeFlex/static/js/2.41ab59f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CodeFlex/static/js/2.41ab59f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b22a4b565141362617fd",
+    "url": "/CodeFlex/static/js/main.7540a4b0.chunk.js"
+  },
+  {
+    "revision": "29dff172430e1272936f",
+    "url": "/CodeFlex/static/js/runtime-main.4b8148de.js"
+  },
+  {
+    "revision": "4946ccfdc6cafb4f9c27ac28448a4f11",
+    "url": "/CodeFlex/static/media/bg.4946ccfd.jpg"
+  }
+]);
